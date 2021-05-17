ba89ff60eb75afac4912f253410e40611ed6d1e9
Eugene Skrobov <skrobov.ev@gmail.com>
2021-05-17 19:48:09 +0700

    deploy

---
+++ _nuxt/static/1621255679/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-clever-baby\u002F_nuxt\u002Fstatic\u002F1621255679",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fnuxt-clever-baby\u002F",assetsPath:"\u002Fnuxt-clever-baby\u002F_nuxt\u002F",cdnURL:a}}}}(null));
